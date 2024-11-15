164c5cb9681ee646b91de9a9044047a9bb92d179
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-11-15 15:34:42 +0300

    change AppRouter paths

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -3,6 +3,8 @@ import {Navigate, Route, Routes} from "react-router-dom";
 import LoginForm from "./pages/login/LoginForm";
 import {AuthContext} from "../index";
 import {observer} from "mobx-react-lite";
+import Main from "./pages/main/Main";
+import RegisterForm from "./pages/register/RegisterForm";
 
 const AppRouter: FC = () => {
     const {store} = useContext(AuthContext)
@@ -16,12 +18,13 @@ const AppRouter: FC = () => {
     return (
         store.isAuth ? (
             <Routes>
-                <Route path="/login" element={<Navigate to={"/main"} />} />
+                <Route path="/main" element={<Main/>}/>
                 <Route path="*" element={<Navigate to={"/main"} />} />
             </Routes>
         ) : (
             <Routes>
                 <Route path="/login" element={<LoginForm />} />
+                <Route path="/register" element={<RegisterForm/>}/>
                 <Route path="*" element={<Navigate to={"/login"} />} />
             </Routes>
         )
