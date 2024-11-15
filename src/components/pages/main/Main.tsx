e039e9ae25e72dd3435fd4f655756d0ef550894c
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-11-15 15:28:56 +0300

    Добавил начальный шаблон для главной страницы

---
+++ src/components/pages/main/Main.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import Navbar from "../../ui/navbar/Navbar";
+
+const Main: FC = () => {
+    return (
+        <div>
+            <Navbar/>
+            Главная страница
+        </div>
+    );
+};
+
+export default Main;
