862ec5c4456b2ac88148a98ff57d737e32e99f7f
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-11-15 15:37:26 +0300

    Fix class

--- src/components/ui/input/LoginInput.tsx
+++ src/components/ui/input/LoginInput.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import cl from './LoginInput.module.css'
+import cl from './LoginInput.module.css';
 
 interface LoginInputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
 const LoginInput: React.FC<LoginInputProps> = (props) => {
+    // Объединяем переданный className с cl.loginInput
+    const combinedClassName = `${cl.loginInput} ${props.className || ''}`;
+
     return (
-        <input {...props} className={cl.loginInput}/>
+        <input {...props} className={combinedClassName} />
     );
 };
 
