e6ab861c8e723b49a5ab90268f16c1387c9da947
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-12-03 22:20:00 +0300

    add to createProperty

--- src/services/PropertyService.ts
+++ src/services/PropertyService.ts
@@ -1,9 +1,14 @@
 import {AxiosResponse} from "axios";
 import {IProperty} from "../models/IProperty";
 import $api from "../http";
+import {AddPropertyRequest} from "../models/request/AddPropertyRequest";
 
 export default class PropertyService {
     static async getPropertiesByOwnerId(ownerId : bigint) : Promise<AxiosResponse<IProperty[]>>  {
         return $api.get(`/properties?ownerId=${ownerId}`)
+    }
+
+    static async createProperty(request: AddPropertyRequest): Promise<AxiosResponse<String>> {
+        return $api.post(`/properties/form`, request)
     }
 }
