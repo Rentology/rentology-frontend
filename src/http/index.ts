4554313535ee4f77b8db829458970eaf3c575c9a
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-12-04 16:32:27 +0300

    add docker

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
-export const API_URL = `http://localhost:80/api/v1`
+export const API_URL = process.env.REACT_APP_API_URL
 
 const $api = axios.create({
     withCredentials: true,
